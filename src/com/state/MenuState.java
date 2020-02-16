9fab3bd9e2cebb65beb4a968732c520a639004cc
the-daniloo <dt220304@gmail.com>
2020-02-16 19:05:16 -0300

    Classes State
    Houve modificações na classe State, adicionando métodos que permitem
    que haja a troca do estado do jogo e adicionando os métodos "tick()"
    e "render()" que permite que cada estado do jogo faça update nas variaveis
    e seja capaz de renderizar o que se pede.
    
    Foram criados as classes GameState e MenuState que "extends" da classe
    State, com seus métodos "tick()" e "render()" ja implementados.

---
+++ src/com/state/MenuState.java
@@ -0,0 +1,19 @@
+package com.state;
+
+import java.awt.Graphics;
+
+public class MenuState extends State {
+	
+	@Override
+	public void tick()
+	{
+		
+	}
+	
+	@Override
+	public void render(Graphics g)
+	{
+		
+	}
+	
+}
--- src/com/state/State.java
+++ src/com/state/State.java
@@ -1,5 +1,25 @@
 package com.state;
 
-public class State {
+import java.awt.Graphics;
 
+public abstract class State {
+	
+	//STATICS
+	public static State currentState = null;
+	
+	public static State getCurrentState()
+	{
+		return currentState;
+	}
+	
+	public static void setCurrentStateTo(State state)
+	{
+		currentState = state;
+	}
+	
+	//OBJECT
+	
+	public abstract void tick();
+	public abstract void render(Graphics g);
+	
 }
