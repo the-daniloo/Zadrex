0aa6386e7e74cac816aa56ae73ce7be8382643cb
Catsyser <carolspidonee@gmail.com>
2020-02-26 17:48:59 -0300

    criacao da classe square

--- src/com/board/Board.java
+++ src/com/board/Board.java
@@ -10,25 +10,14 @@ public class Board {
 	public static final int SQUARE_HEIGHT = 8;
 	
 	//tamanho do tabuleiro
-	private int height = 512;
-	private int width = 512;
+	private static final int HEIGHT = 512;
+	private static final int WIDTH = 512;
 	
-	//tamanho em PIXELS dos quadrados
-	private int widthSquare = 64;
-	private int heightSquare = 64;
-	
-	
-	private final int black = 0;
-	private final int white = 1;
+	private final int black = 1;
+	private final int white = 0;
 	private int squares[][];
 	
 	public Board() {
-		this.squares = new int[SQUARE_WIDTH][SQUARE_HEIGHT];
-	}
-	
-	public Board (int height, int width) {
-		this.height = height;
-		this.width = width;
 		this.squares = new int[SQUARE_WIDTH][SQUARE_HEIGHT];
 	}
 	
@@ -37,38 +26,30 @@ public class Board {
 	}
 	
 	public void render(Graphics g) {
-		for(int x = 0; x <= 8; x++) {
-			for(int y = 0; y <= 8; y++) {
-					if(squares[x][y] == squares[0][0]) {
-						g.setColor(Color.black);
-						g.fillRect(0, 0, widthSquare, heightSquare);
-					} else {
-					if(squares[x][y] == black) {
-						g.setColor(Color.black);
-						g.fillRect(0, squares[x][y-1] + heightSquare, widthSquare, heightSquare);
-					} else {
-						g.setColor(Color.white);
-						g.fillRect(squares[x-1][y] + widthSquare, 0, widthSquare, heightSquare);
-					}
+		for(int x = 0; x < 8; x++) {
+			for(int y = 0; y < 8; y++) {
+				if(squares[x][y] == black) {
+					g.setColor(Color.black);
+					//g.fillRect();
+				} else {
+					g.setColor(Color.white);
+					//g.fillRect();
 				}
 			}
 		}
 	}
 	
 	public void squareLoad() {
-		for(int x = 0; x <= 8; x++) {
-			for(int y = 0; y <= 8; y++) {
-				if((x % 2 == 0) && (y % 2 == 0)) {
+		boolean blackStatus = true;
+		
+		for(int x = 0; x < 8; x++) {
+			for(int y = 0; y < 8; y++) {
+				if(blackStatus == true) {
 					squares[x][y] = black;
-				}
-				else if((x % 2 != 0) && (y % 2 == 0)) {
+					blackStatus = false;
+				} else {
 					squares[x][y] = white;
-				}
-				else if((x % 2 != 0) && (y % 2 != 0)) {
-					squares[x][y] = black;
-				}
-				else if((x % 2 == 0) && (y % 2 != 0)) {
-					squares[x][y] = white;
+					blackStatus = true;
 				}
 			}
 		}
